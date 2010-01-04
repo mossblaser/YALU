af149f1d92b72aa893a47a49c1a901ce824e8c58
Jonathan Heathcote <jonathan@jonathan-laptop.(none)>
2010-01-04 12:54:06 +0000

    Initial commit

--- help.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-# Yet Another Level Up: FVWM configuration "system"
-#     ~ by Jonathan Heathcote 2009, Licence: GPLv3
-#
-# Filename: help.py
-# Purpose: On-line help and documentation system
